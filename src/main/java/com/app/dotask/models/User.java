76087250723166b8e52606d005f3a0d199f873e0
saucekode <saucekoder@gmail.com>
2020-12-30 21:20:33 +0100

    changed collection in userController to users

--- src/main/java/com/app/dotask/models/User.java
+++ src/main/java/com/app/dotask/models/User.java
@@ -7,8 +7,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.HashSet;
-import java.util.Set;
 
 @Document(collection = "users")
 @Data
